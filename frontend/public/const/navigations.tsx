4e24c1bd0781ad4771a76ea372b60aac0cc53f02
Minstriz <dominhtri1788@gmail.com>
2024-11-06 17:07:19 +0700

    build layout home, add fonts, fix module

--- frontend/public/const/navigations.tsx
+++ frontend/public/const/navigations.tsx
@@ -4,37 +4,37 @@ export const DASHBOARD_SIDEBAR_LINKS = [
     {
         key: 'dashboard',
         label: 'Dashboard',
-        path: '/dashboard',
-        icon: <Home />
+        path: '/admin/dashboard',
+        icon: <Home/>
     },
     {
         key: 'Products',
         label: 'Products',
-        path: '/products',
+        path: '/admin/products',
         icon: <Package />
     },
     {
         key: 'Orders',
         label: 'Orders',
-        path: '/orders',
+        path: '/admin/orders',
         icon: <ShoppingCart />
     },
     {
         key: 'Customers',
         label: 'Customers',
-        path: '/customers',
+        path: '/admin/customers',
         icon: <Users />
     },
     {
         key: 'Transactions',
         label: 'Transactions',
-        path: '/transactions',
+        path: '/admin/transactions',
         icon: <LineChart />
     },
     {
         key: 'Messengers',
         label: 'Messengers',
-        path: '/messengers',
+        path: '/admin/messengers',
         icon: <Bell />
     }
 ]
---
+++ frontend/src/app/admin/page.tsx
@@ -0,0 +1,23 @@
+"use client"
+import AdminHeader from "@/components/features/admin-header"
+import AdminSidebar from "@/components/features/admin-sidebar"
+
+export default function Admin({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
+            <div className="hidden border-r bg-muted/40 md:block">
+                <AdminSidebar />
+            </div>
+            <div className="flex flex-col">
+                <AdminHeader />
+                <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
+                    {children}
+                </main>
+            </div>
+        </div>
+    )
+}
