4e6d5ce248c3374e91f1104296f8b3ec7eebea43
Minstriz <dominhtri1788@gmail.com>
2024-11-27 18:43:44 +0700

    update navbar

--- frontend/src/components/features/navbar.tsx
+++ frontend/src/components/features/navbar.tsx
@@ -109,7 +109,7 @@ export default function Navbar() {
                   href="#"
                   className="text-[18px] block py-2 pl-3 pr-4 text-white border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-yellow-300 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
                 >
-                  Marketplace
+                  Collection
                 </a>
               </li>
               <li>
@@ -117,7 +117,7 @@ export default function Navbar() {
                   href="#"
                   className="text-[18px] block py-2 pl-3 pr-4 text-white border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-yellow-300 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
                 >
-                  Features
+                  Order Artwork
                 </a>
               </li>
               <li>
